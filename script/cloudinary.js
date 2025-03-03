efa676adc09d755f5be02fccf83ce59454a0cb8e
LilBlood16 <jaylua2548@gmail.com>
2025-03-03 18:47:02 +0800

    Add files via upload

---
+++ script/cloudinary.js
@@ -0,0 +1,128 @@
+//* source https://codepen.io/team/Cloudinary/pen/QgpyOK
+
+const cloudName = 'lilx-confession';
+const unsignedUploadPreset = 'Lilx-Confession';
+
+var fileSelect = document.getElementById("fileSelect")
+var fileElem = document.getElementById("fileElem")
+
+fileSelect.addEventListener("click", function (e) {
+    if (fileElem) {
+        fileElem.click();
+    }
+    e.preventDefault(); // prevent navigation to "#"
+}, false);
+
+// ************************ Drag and drop ***************** //
+function dragenter(e) {
+    e.stopPropagation();
+    e.preventDefault();
+}
+
+function dragover(e) {
+    e.stopPropagation();
+    e.preventDefault();
+}
+
+dropbox = document.getElementById("post-write");
+dropbox.addEventListener("dragenter", dragenter, true);
+dropbox.addEventListener("dragover", dragover, false);
+dropbox.addEventListener("drop", drop, false);
+
+function drop(e) {
+    e.stopPropagation();
+    e.preventDefault();
+
+    var dt = e.dataTransfer;
+    var files = dt.files;
+    var fileCheck = files[0].name.split('.')
+    var fileType = fileCheck[1]
+    var imageType = ['jpg', 'png', 'gif', 'jpeg', 'TIFF', 'EPS', 'AI', 'RAW', 'INDD', 'PDF']
+    // console.log(fileType)
+    var checkIsImg = imageType.includes(fileType)
+    // console.log(checkIsImg)
+    if (checkIsImg == true) {
+        handleFiles(files);
+    } else if (checkIsImg == false) {
+        document.getElementById('upload-warn').style.display = "block"
+    }
+}
+
+// *********** Upload file to Cloudinary ******************** //
+function uploadFile(file) {
+    document.getElementById('upload-warn').style.display = "none"
+    var url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
+    var xhr = new XMLHttpRequest();
+    var fd = new FormData();
+    xhr.open('POST', url, true);
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+
+    // Reset the upload progress bar
+    document.getElementById('progress').style.display = "block";
+    document.getElementById('progress').style.width = 0;
+
+    // Update progress (can be used to show progress indicator)
+    xhr.upload.addEventListener("progress", function (e) {
+        var progress = Math.round((e.loaded * 100.0) / e.total);
+        document.getElementById('progress').style.width = progress + "%";
+    })
+
+    xhr.onreadystatechange = function (e) {
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            // File uploaded successfully
+            var response = JSON.parse(xhr.responseText);
+            // https://res.cloudinary.com/cloudName/image/upload/v1483481128/public_id.jpg
+            var url = response.secure_url;
+            console.log(url)
+
+            document.getElementById('img-content').src = url
+            document.getElementById('img-content').style.display = "block"
+            document.getElementById('progress').style.display = "none";
+            document.getElementById('remove-upload').style.display = "block"
+            // //* split to check file type
+            // var urlSplit = url.split('.')
+            // var imageType = ['jpg', 'png', 'gif', 'jpeg', 'TIFF', 'EPS', 'AI', 'RAW', 'INDD', 'PDF']
+
+            // // ? if false, the file we receive is video, if true we receive image
+            // var isImg = imageType.includes(urlSplit[length - 1])
+
+            // if (isImg == true) {
+            //     document.getElementById('img-content').src = url
+            //     document.getElementById('img-content').style.display = "block"
+            // } else if (isImg == false) {
+            //     document.getElementById('vid-content').src = url
+            //     document.getElementById('vid-content').style.display = "block"
+            // }
+
+            //* Implement another technique----------------------------------------------------------------------------------------------------------------------
+            // var img = new Image(); // HTML5 Constructor
+            // img.src = url;
+            // img.alt = response.public_id;
+            // img.className = "img-content";
+            // img.id = "img-content"
+            // checkImg = document.getElementById("post-img").classList.contains("img-content")
+            // //   console.log(checkImg)
+            // //* check if image has already uploaded? if already uploaded we will remove old image and preview the new upload image
+            // if (checkImg == false) {
+            //     document.getElementById('post-img').appendChild(img)
+            // } else if (checkImg == true) {
+            //     //* remove old image and add a new one
+            //     document.querySelector('post-img').innerHTML = ''
+            //     document.getElementById('post-img').appendChild(img)
+            // }
+            //*-------------------------------------------------------------------------------------------------------------------------------------------------------
+        }
+    };
+
+    fd.append('upload_preset', unsignedUploadPreset);
+    fd.append('tags', 'browser_upload'); // Optional - add tag for image admin in Cloudinary
+    fd.append('file', file);
+    xhr.send(fd);
+}
+
+// *********** Handle selected files ******************** //
+var handleFiles = function (files) {
+    for (var i = 0; i < files.length; i++) {
+        uploadFile(files[i]); // call the function to upload the file
+    }
+};
---
+++ script/confession.js
@@ -0,0 +1,393 @@
+var postData = []
+const profile = [
+    "./asset/profile-black.png",
+    "./asset/profile-white.png",
+    "./asset/profile-orange.png",
+    "./asset/profile-pink.png",
+    "./asset/profile-red.png",
+    "./asset/profile-green.png"
+]
+
+var formatter = Intl.NumberFormat('en', { notation: 'compact'})
+
+// ? when window on load we generate posts and comments
+window.onload = async function () {
+    generateProfile()
+    await generateData()
+    // generateComment()
+}
+
+function fetchData() {
+    return fetch('').then((res) => {
+        return res.json()
+    }).then((data) => {
+        return data
+    })
+}
+
+async function generateData() {
+    postData = []
+    await fetchData().then(function (result) {
+        for (let i = result.length - 1; i >= 0; i--) {
+            postData.push(result[i]);
+        }
+    }).then(() => {
+        // ? after pushing data to our array we start generate the post
+        console.log(postData)
+        genertatePost()
+    }).then(() => {
+        generateComment()
+    })
+}
+
+function genertatePost() {
+    for (var i = 0; i < postData.length; i++) {
+        let randomNumber = randomNum(0, 5)
+        if (postData[i].image == "") {
+            document.querySelector('.post-container').innerHTML += `
+            <div class="post-box">
+                <div class="profile">
+                    <img src="${profile[randomNumber]}" alt="profile">
+                    <span class="imposter">Impostor</span>
+                    <span class="imposter profile-id">#${postData[i].impostorId}</span>
+                    <span class="imposter dot">•</span>
+                    <span class="imposter post-time">${postData[i].date}</span>
+                </div>
+                <div class="post-content" id="imp#${postData[i].impostorId}">
+                    <div class="div-post-text">
+                        <span class="post-text">${postData[i].text}</span>
+                    </div>
+                    <div class="reaction">
+                        <div class="sus reaction-align">
+                            <img onclick=upvote(id) id="up#${postData[i].impostorId}" src="./asset/sus.png" alt="reaction">
+                            <span class="sus-reaction" id="upvote#${postData[i].impostorId}">${formatter.format(postData[i].upvote)}</span>
+                        </div>
+                        <div class="downvote reaction-align">
+                            <img onclick=downvote(id) id="down#${postData[i].impostorId}" src="./asset/voted.png" alt="reaction">
+                            <span class="vote-reaction" id="downvote#${postData[i].impostorId}">${formatter.format(postData[i].downvote)}</span>
+                        </div>
+                        <div class="comment reaction-align">
+                            <span class="comment-btn" id="comment#${postData[i].impostorId}">0 Comment</span>
+                        </div>
+                        <div class="report reaction-align">
+                            <img onclick=reportvote(id) id="report#${postData[i].impostorId}" src="./asset/report.png" alt="reaction">
+                            <span class="report-reaction" id="share#${postData[i].impostorId}">${formatter.format(postData[i].share)}</span>
+                        </div>
+                    </div>
+                    <div class="comment-area">
+                        <form class="comment-form">
+                            <textarea oninput=auto_grow(this) class="comment-text"
+                                placeholder="Write a public comment..." id="content#${postData[i].impostorId}"></textarea>
+                        </form>
+                        <img onclick=sendCmt(id) id="send#${postData[i].impostorId}" class="send" src="./asset/send.png" alt="send">
+                    </div>
+                    <div class="commented" id="cmt#${postData[i].impostorId}">
+                    </div>
+                </div>
+            </div>
+        `
+        } else if (postData[i].image != "") {
+            document.querySelector('.post-container').innerHTML += `
+            <div class="post-box">
+                <div class="profile">
+                    <img src="${profile[randomNumber]}" alt="profile">
+                    <span class="imposter">Impostor</span>
+                    <span class="imposter profile-id">#${postData[i].impostorId}</span>
+                    <span class="imposter dot">•</span>
+                    <span class="imposter post-time">${postData[i].date}</span>
+                </div>
+                <div class="post-content" id="imp#${postData[i].impostorId}">
+                    <div class="div-post-text">
+                        <span class="post-text">${postData[i].text}</span>
+                    </div>
+                    <img class="post-image" src="${postData[i].image}" alt="image">
+                    <div class="reaction">
+                        <div class="sus reaction-align">
+                            <img onclick=upvote(id) id="up#${postData[i].impostorId}" src="./asset/sus.png" alt="reaction">
+                            <span class="sus-reaction" id="upvote#${postData[i].impostorId}">${formatter.format(postData[i].upvote)}</span>
+                        </div>
+                        <div class="downvote reaction-align">
+                            <img onclick=downvote(id) id="down#${postData[i].impostorId}" src="./asset/voted.png" alt="reaction">
+                            <span class="vote-reaction" id="downvote#${postData[i].impostorId}">${formatter.format(postData[i].downvote)}</span>
+                        </div>
+                        <div class="comment reaction-align">
+                            <span class="comment-btn" id="comment#${postData[i].impostorId}">0 Comment</span>
+                        </div>
+                        <div class="report reaction-align">
+                            <img onclick=reportvote(id) id="report#${postData[i].impostorId}" src="./asset/report.png" alt="reaction">
+                            <span class="report-reaction" id="share#${postData[i].impostorId}">${formatter.format(postData[i].share)}</span>
+                        </div>
+                    </div>
+                    <div class="comment-area">
+                        <form class="comment-form">
+                            <textarea oninput=auto_grow(this) class="comment-text"
+                                placeholder="Write a public comment..." id="content#${postData[i].impostorId}"></textarea>
+                        </form>
+                        <img onclick=sendCmt(id) id="send#${postData[i].impostorId}" class="send" src="./asset/send.png" alt="send">
+                    </div>
+                    <div class="commented" id="cmt#${postData[i].impostorId}">
+                    </div>
+                </div>
+            </div>
+        `
+        }
+    }     
+}
+
+function generateProfile() {
+    document.querySelector('.hub-logo').src = profile[randomNum(0, 5)]
+    document.querySelector('.prof-logo').src = profile[randomNum(0, 5)]
+}
+
+function generateComment() {
+    for (var i = postData.length - 1; i >= 0; i--) {
+        // console.log(postData[i].comment)
+
+        //* https://stackoverflow.com/questions/24403732/how-to-check-if-array-is-empty-or-does-not-exist
+
+        if (postData[i].comment?.length) {
+            // ? check size of comment then proceed to render comment
+            // console.log(postData[i].comment)
+            for (var j = 0; j < postData[i].comment.length; j++) {
+                var tempText = JSON.stringify(postData[i].comment[j].comments)
+                // ? remove first and last character
+                tempText = tempText.substring(1, tempText.length - 1)
+
+                document.getElementById(`cmt#${postData[i].impostorId}`).innerHTML += `
+                <div class="cmt-text" id="cmted#${j + 1}">
+                <img src="${profile[randomNum(0, 5)]}" alt="profile">
+                <span> ${tempText}
+                </span>
+                </div>
+                `
+
+                tempText = ''
+            }
+            if (postData[i].comment.length == 0 || postData[i].comment.length == 1) {
+                document.getElementById(`comment#${postData[i].impostorId}`).innerHTML = formatter.format(postData[i].comment.length) + " comment"
+            } else if (postData[i].comment.length > 1) {
+                document.getElementById(`comment#${postData[i].impostorId}`).innerHTML = formatter.format(postData[i].comment.length) + " comments"
+            }
+        }
+    }
+    console.log("Done")
+}
+
+//*Credit https://stackoverflow.com/questions/17772260/textarea-auto-height
+function auto_grow(element) {
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight) + "px";
+}
+
+function findPostIndex(impID, allData) {
+    return allData.findIndex((post) => post.impostorId == impID)
+}
+
+async function sendCmt(event) {
+    var commentArr = [{}]
+    var textArr = {}
+    var cmtId = ''
+    var ranNum = randomNum(0, 5)
+    var impId = event.replace('send#', '')
+    var totalCmt = ''
+    var cmtText = document.getElementById(`content#${impId}`).value
+    if (cmtText != "") {
+        await fetchData().then(function (allData) {
+            console.log(allData)
+            const index = findPostIndex(impId, allData)
+            console.log(index)
+            // ? get array of comment and then we put object into temp array then push to array of comment and start patching on database
+            commentArr = allData[index].comment
+            // console.log(commentArr)
+            textArr = { comments: cmtText }
+            commentArr.push(textArr)
+            cmtId = commentArr.length
+            console.log(commentArr)
+        })
+
+        try {
+            await axios.patch(`https://yatoconfessionapi77.netlify.app/.netlify/functions/api/post/by_id/${impId}`, {
+                comment: commentArr
+            }).then((res) => {
+                console.log("Comment has been posted")
+                // console.log(res)
+                return res
+            })
+        } catch (err) {
+            console.log(err)
+        }
+
+        document.getElementById(`cmt#${impId}`).innerHTML += `
+        <div class="cmt-text" id="cmted#${cmtId}">
+        <img src="${profile[ranNum]}" alt="profile">
+        <span> ${cmtText}
+        </span>
+        </div>
+        `
+    } else {
+        console.log('Comment has no content! Send denied')
+    }
+
+    // ? after post to api we change text area to blank and change comment plural
+
+    document.getElementById(`content#${impId}`).value = ''
+    document.getElementById(`content#${impId}`).style.height = '20px'
+
+    totalCmt = document.getElementById(`comment#${impId}`).innerText
+    totalCmt = totalCmt.replace(' comments', '')
+    totalCmt = parseInt(totalCmt)
+    totalCmt += 1
+    // console.log(totalCmt)
+
+    if (totalCmt <= 1) {
+        document.getElementById(`comment#${impId}`).innerHTML = formatter.format(totalCmt) + " comment"
+    } else if (totalCmt > 1) {
+        document.getElementById(`comment#${impId}`).innerHTML = formatter.format(totalCmt) + " comments"
+    }
+}
+
+async function postContent(data) {
+    // try {
+    //     fetch('https://yatoconfessionapi77.netlify.app/.netlify/functions/api/post', {
+    //         method: 'POST',
+    //         body: JSON.stringify(data),
+    //         headers: {
+    //             'Content-type': 'application/json; charset=UTF-8',
+    //         }
+    //     }).then(function (res) {
+    //         // console.log(res)
+    //         return res.json()
+    //     }).then(() => {
+    //         console.log("API has been posted")
+    //     })
+    // } catch (err) {
+    //     console.log(err)
+    // }
+    try {
+        await axios.post('https://yatoconfessionapi77.netlify.app/.netlify/functions/api/post', data).then((res) => {
+            console.log("API has been posted")
+            return res
+        })
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+function randomNum(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min)
+}
+
+async function confess() {
+    var currentPostID
+    var contentData = []
+    var confessionContent = document.getElementById('confess-box').value
+    var imgLink = ''
+    var upvotes = '0'
+    var downvotes = '0'
+    var shares = '0'
+    var pins = '0000'
+
+    imgLink = document.getElementById('img-content').src
+
+    if (confessionContent != "" || imgLink != '') {
+        await fetchData().then(function (allData) {
+            currentPostID = allData[allData.length - 1].impostorId
+            currentPostID = parseInt(currentPostID)
+            currentPostID += 1
+            contentData = {
+                text: confessionContent,
+                image: imgLink,
+                upvote: upvotes,
+                downvote: downvotes,
+                share: shares,
+                pin: pins,
+                impostorId: currentPostID
+            }
+        })
+        //* wait for content to store in database before render to the client postContent(contentData)
+        //* https://laracasts.com/discuss/channels/javascript/how-to-properly-use-async-await-with-axios-post-request
+        const apiPost = await postContent(contentData)
+
+        //* reset write content
+        document.getElementById('confess-box').value = ''
+        document.querySelector('.post-container').innerHTML = ''
+        document.getElementById('img-content').src = ''
+        document.getElementById('img-content').style.display = 'none'
+        // document.getElementById('vid-content').src = ''
+        // document.getElementById('vid-content').style.display = 'none'
+        document.getElementById('remove-upload').style.display = "none"
+        document.getElementById('img-content').removeAttribute('src')
+        generateData()
+        // location.reload()
+    }
+}
+
+async function upvote(impID) {
+    impID = impID.replace('up#', '')
+    await fetchData().then(function (allData) {
+        const index = findPostIndex(impID, allData)
+        try {
+            if (allData[index].impostorId == impID) {
+                axios.patch(`https://yatoconfessionapi77.netlify.app/.netlify/functions/api/post/by_id/${impID}`, {
+                    upvote: allData[index].upvote + 1
+                }).then((res) => {
+                    console.log("Upvoted")
+                    // console.log(res)
+                    return res
+                })
+                document.getElementById(`upvote#${impID}`).innerHTML = formatter.format(allData[index].upvote + 1)
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    })
+}
+
+async function downvote(impID) {
+    impID = impID.replace('down#', '')
+    await fetchData().then(function (allData) {
+        const index = findPostIndex(impID, allData)
+        try {
+            if (allData[index].impostorId == impID) {
+                axios.patch(`https://yatoconfessionapi77.netlify.app/.netlify/functions/api/post/by_id/${impID}`, {
+                    downvote: allData[index].downvote - 1
+                }).then((res) => {
+                    console.log("Downvoted")
+                    // console.log(res)
+                    return res
+                })
+                document.getElementById(`downvote#${impID}`).innerHTML = formatter.format(allData[index].downvote - 1)
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    })
+}
+
+async function reportvote(impID) {
+    impID = impID.replace('report#', '')
+    await fetchData().then(function (allData) {
+        const index = findPostIndex(impID, allData)
+        try {
+            if (allData[index].impostorId == impID) {
+                axios.patch(`https://yatoconfessionapi77.netlify.app/.netlify/functions/api/post/by_id/${impID}`, {
+                    share: allData[index].share + 1
+                }).then((res) => {
+                    console.log("Reported")
+                    // console.log(res)
+                    return res
+                })
+                document.getElementById(`share#${impID}`).innerHTML = formatter.format(allData[index].share + 1)
+            }
+        } catch (err) {
+            console.log(err)
+        }
+    })
+}
+
+function removeImg() {
+    document.getElementById('img-content').removeAttribute('src')
+    document.getElementById('img-content').style.display = 'none'
+    // document.getElementById('vid-content').src = ''
+    document.getElementById('remove-upload').style.display = "none";
+}
